cdd67f89a98abb433ddfdc55aa6b3b0a8142d424
TasbeehMahmoud30 <120498537+TasbeehMahmoud30@users.noreply.github.com>
2022-12-22 13:48:47 +0200

    Update MyOnlineShop2.java

--- MyOnlineShop2/src/main/java/com/mycompany/myonlineshop2/MyOnlineShop2.java
+++ MyOnlineShop2/src/main/java/com/mycompany/myonlineshop2/MyOnlineShop2.java
@@ -2,6 +2,21 @@ package com.mycompany.myonlineshop2;
 public class MyOnlineShop2 {
 
     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter the number of books : ");
+        int x = input.nextInt();
+        CartoonBook[] arr = new CartoonBook[x]; 
+        CartoonBook b = new CartoonBook();
+        double Totalprice =0;
+        for(int i=0 ; i<x ; i++){
+             System.out.print("Enter the price of CartoonBook : ");   
+             double price = input.nextInt();
+             b.setPrice(price) ;
+              arr[i]= b;
+             Totalprice=Totalprice + price;
+        System.out.println("The array : " + arr[i].price);
+        }
+        System.out.println("The total price of books is : " + Totalprice);
  
     }
 }
